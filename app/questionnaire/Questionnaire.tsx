e3c5a6f988bac1d18dca0a566c44fdb2fe83d773
Pruthvi <dhannapuneni@outlook.com>
2024-09-03 21:16:24 -0400

    Add the review screen before submitting.

--- app/questionnaire/Questionnaire.tsx
+++ app/questionnaire/Questionnaire.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { gql, useQuery, useMutation } from "@apollo/client";
 import QuestionnaireCard from "./components/QuestionnaireCard";
 import QuestionnaireNavigation from "./components/QuestionnaireNavigation";
-import { CircularProgress, LinearProgress, Typography } from "@mui/material";
+import { CircularProgress, Divider, LinearProgress, List, ListItem, ListItemText, Paper, Typography } from "@mui/material";
 import Question from "./components/Question";
 import { GET_QUESTIONS } from "../lib/graphql/queries/questionsQuery";
 import { SUBMIT_ANSWERS } from "../lib/graphql/mutations/questionsMutation";
@@ -23,11 +23,24 @@ const Questionnaire = () => {
     const { loading: questionsLoading, error, data } = useQuery(GET_QUESTIONS);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [answers, setAnswers] = useState<Record<string, any>>({});
+    const [isReviewing, setIsReviewing] = useState(false);
+    const [review, setReview] = useState(false);
+    const [reviewData, setReviewData] = useState<{ question: string; answer: string }[]>([]);
     const [isSubmitted, setIsSubmitted] = useState(false);
     const [submissionMessage, setSubmissionMessage] = useState<string | null>(null);
     const [showError, setShowError] = useState(false);
     const [submitAnswers, { loading: isSubmitting, error: submitError }] = useMutation(SUBMIT_ANSWERS);
     
+
+    useEffect(() => {
+        if ( data && currentQuestionIndex === questions.length - 1) {
+            setReview(true);
+        }
+        else {
+            setReview(false);
+        }
+    }, [currentQuestionIndex]);
+
 
     if (questionsLoading || isSubmitting) {
         const message = questionsLoading ? "Loading questions..." : "Submitting answers...";
@@ -93,12 +106,16 @@ const Questionnaire = () => {
 
     const handleBack = () => {
         setShowError(false);
-        if (currentQuestionIndex > 0) {
-            setCurrentQuestionIndex((currentQuestionIndex) => currentQuestionIndex - 1);
+        if (isReviewing) {
+            setIsReviewing(false);
+            setReview(true);
+        } else if (currentQuestionIndex > 0) {
+            setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
         }
     };
 
     const handleSubmit = async (answers: Record<string, any>) => {
+        setIsReviewing(false);
         console.log("Submitted Answers: ", answers);
         const formattedAnswers = Object.entries(answers).map(([questionId, answer]) => ({
           questionId,
@@ -117,6 +134,62 @@ const Questionnaire = () => {
         }
     };
 
+    const handleReview = (answers: Record<string, any>) => {
+        setIsReviewing(true);
+        setReview(false);
+        
+        // Create an array to store all questions and answers
+        const reviewData = questions.map((question) => ({
+            question: question.question,
+            answer: answers[question.id] || 'Not answered'
+        }));
+        console.log("Review Data: ", reviewData);
+        // Set the review data in the state
+        setReviewData(reviewData);
+    };
+
+    const renderReviewAnswers = (reviewData: { question: string; answer: string | string[] | File | null }[]): React.ReactElement => {
+        const formatAnswer = (answer: string | string[] | File | null) => {
+            if (typeof answer === 'object' && answer !== null) {
+                if (Array.isArray(answer)) {
+                    return answer.join(', ');
+                } else if (answer instanceof File) {
+                    return answer.name;
+                } else {
+                    return JSON.stringify(answer);
+                }
+            }
+            return String(answer);
+        };
+
+        return (
+            <Paper elevation={1} sx={{ maxHeight: '50vh', overflow: 'auto', padding: 2, marginBottom: '20px' }}>
+                <Typography variant="h6" gutterBottom>Review Your Answers</Typography>
+                <List>
+                    {reviewData.map((item, index) => (
+                        <React.Fragment key={index}>
+                            <ListItem alignItems="flex-start">
+                                <ListItemText
+                                    primary={<Typography variant="subtitle1" color="primary">{item.question}</Typography>}
+                                    secondary={
+                                        <Typography
+                                            component="span"
+                                            variant="body2"
+                                            color="text.primary"
+                                        >
+                                            {formatAnswer(item.answer)}
+                                        </Typography>
+                                    }
+                                />
+                            </ListItem>
+                            {index < reviewData.length - 1 && <Divider component="li" />}
+                        </React.Fragment>
+                    ))}
+                </List>
+            </Paper>
+        );
+    };
+
     return (
         <QuestionnaireCard>
             {isSubmitted ? (
@@ -125,24 +198,29 @@ const Questionnaire = () => {
                 </Typography>
             ) : (
                 <>
-                    <LinearProgress
-                        variant="determinate"
-                        value={((currentQuestionIndex + 1) / questions.length) * 100}
-                        sx={{ 
-                            marginBottom: '20px',
-                            backgroundColor: 'lightgrey',
-                        }}
-                        
-                    />
-                    <div style={{ paddingTop: '20px', overflow: 'auto', maxHeight: '60vh' }}>
-                        <div style = {{border: showError ? '1px solid red' : 'none', padding: '5px', borderRadius: '5px'}}>
-                            <Question
-                                question={questions[currentQuestionIndex]}
-                                answer={answers[questions[currentQuestionIndex]?.id] || ''}
-                                onAnswerChange={handleAnswerChange}
+                    {isReviewing ? (
+                        renderReviewAnswers(reviewData)
+                    ) : (
+                        <>
+                            <LinearProgress
+                                variant="determinate"
+                                value={((currentQuestionIndex + 1) / questions.length) * 100}
+                                sx={{ 
+                                    marginBottom: '20px',
+                                    backgroundColor: 'lightgrey',
+                                }}
                             />
-                        </div>
-                    </div>
+                            <div style={{ paddingTop: '20px', overflow: 'auto', maxHeight: '60vh' }}>
+                                <div style={{border: showError ? '1px solid red' : 'none', padding: '5px', borderRadius: '5px'}}>
+                                    <Question
+                                        question={questions[currentQuestionIndex]}
+                                        answer={answers[questions[currentQuestionIndex]?.id] || ''}
+                                        onAnswerChange={handleAnswerChange}
+                                    />
+                                </div>
+                            </div>
+                        </>
+                    )}
                     <div style={{ position: 'absolute', bottom: 0, left: 0, right: 0, padding: '20px', marginTop: '10px' }}>
                         <QuestionnaireNavigation
                             hasPrevious={currentQuestionIndex > 0}
@@ -155,6 +233,8 @@ const Questionnaire = () => {
                                     setShowError(false);
                                     if (currentQuestionIndex < questions.length - 1) {
                                         handleNext();
+                                    } else if(review) {
+                                        handleReview(answers);
                                     } else {
                                         handleSubmit(answers);
                                     }
@@ -162,14 +242,14 @@ const Questionnaire = () => {
                             }}
                             backButtonText="← Back"
                             nextButtonText={
-                                currentQuestionIndex < questions.length - 1 ? "Next →" : "Submit"
+                                currentQuestionIndex < questions.length - 1 ? "Next →" : (review ? "Review" : "Submit")
                             }
                         />
                     </div>
                 </>
             )}
         </QuestionnaireCard>
-    )       
+    );
 }
 
-export default Questionnaire
+export default Questionnaire;
